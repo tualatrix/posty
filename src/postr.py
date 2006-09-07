6ed48c43de70553996bee90526ed0389324a3f1a
Ross Burton <ross@burtonini.com>
2006-09-07 22:30:20 +0100

    Start refactoring the source

--- src/postr.py
+++ src/postr.py
@@ -46,141 +46,191 @@ token = fapi.getToken(browser="epiphany -p", perms="write")
  COL_TITLE,
  COL_DESCRIPTION,
  COL_TAGS) = range (0, 6)
-model = gtk.ListStore (gobject.TYPE_STRING, gtk.gdk.Pixbuf, gtk.gdk.Pixbuf, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING)
+
+# TODO: move into Postr
 current_it = None
 
-glade = gtk.glade.XML(os.path.join (os.path.dirname(__file__), "postr.glade"))
-# TODO: autoconnect
+from Queue import Queue
+upload_queue = Queue()
 
-window = glade.get_widget("main_window")
 
-# TODO: hook into state of upload and confirm quit if still uploading
-window.connect('destroy', gtk.main_quit)
-glade.get_widget('quit').connect('activate', gtk.main_quit)
+class AboutDialog(gtk.AboutDialog):
+    def __init__(self, parent):
+        gtk.AboutDialog.__init__(self)
+        self.set_transient_for(parent)
+        self.set_name('Flickr Uploader')
+        self.set_copyright(u'Copyright \u00A9 2006 Ross Burton')
+        self.set_authors(('Ross Burton <ross@burtonini.com>',))
+        self.set_website('http://burtonini.com/')
 
-thumbnail_image = glade.get_widget('thumbnail_image')
-title_entry = glade.get_widget("title_entry")
-desc_entry = glade.get_widget("desc_entry")
-tags_entry = glade.get_widget("tags_entry")
 
-def on_field_changed(entry, column):
-    global current_it
-    # We often get called when there is no iterator as we've just cleared the
-    # field
-    if current_it is None: return
-    model.set_value (current_it, column, entry.get_text())
-title_entry.connect('changed', on_field_changed, COL_TITLE)
-desc_entry.connect('changed', on_field_changed, COL_DESCRIPTION)
-tags_entry.connect('changed', on_field_changed, COL_TAGS)
+class Postr:
+    def __init__(self):
+        glade = gtk.glade.XML(os.path.join (os.path.dirname(__file__), "postr.glade"))
+        glade.signal_autoconnect(self)
 
-iconview = glade.get_widget("iconview")
-iconview.set_model (model)
-iconview.set_text_column (COL_TITLE)
-iconview.set_pixbuf_column (COL_THUMBNAIL)
+        self.window = glade.get_widget("main_window")
 
-iconview.drag_dest_set (gtk.DEST_DEFAULT_ALL, (), gtk.gdk.ACTION_COPY)
-targets = ()
-targets = gtk.target_list_add_image_targets(targets, DRAG_IMAGE, False)
-targets = gtk.target_list_add_uri_targets(targets, DRAG_URI)
-iconview.drag_dest_set_target_list(targets)
+        # TODO: hook into state of upload and confirm quit if still uploading
+        self.window.connect('destroy', gtk.main_quit)
+        glade.get_widget('quit').connect('activate', gtk.main_quit)
 
-def on_selection_changed(iconview):
-    global current_it
+        self.thumbnail_image = glade.get_widget('thumbnail_image')
+        self.title_entry = glade.get_widget("title_entry")
+        self.desc_entry = glade.get_widget("desc_entry")
+        self.tags_entry = glade.get_widget("tags_entry")
+
+        self.model = gtk.ListStore (gobject.TYPE_STRING, gtk.gdk.Pixbuf, gtk.gdk.Pixbuf, gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING)
+
+        def on_field_changed(entry, column):
+            global current_it
+            # We often get called when there is no iterator as we've just
+            # cleared the field
+            if current_it is None: return
+            self.model.set_value (current_it, column, entry.get_text())
+
+        self.title_entry.connect('changed', on_field_changed, COL_TITLE)
+        self.desc_entry.connect('changed', on_field_changed, COL_DESCRIPTION)
+        self.tags_entry.connect('changed', on_field_changed, COL_TAGS)
+
+        self.iconview = glade.get_widget("iconview")
+        self.iconview.set_model (self.model)
+        self.iconview.set_text_column (COL_TITLE)
+        self.iconview.set_pixbuf_column (COL_THUMBNAIL)
     
-    items = iconview.get_selected_items()
-    if len(items) > 1:
-        print "Unexpected number of selections"
-        return
+        self.iconview.drag_dest_set (gtk.DEST_DEFAULT_ALL, (), gtk.gdk.ACTION_COPY)
+        targets = ()
+        targets = gtk.target_list_add_image_targets(targets, DRAG_IMAGE, False)
+        targets = gtk.target_list_add_uri_targets(targets, DRAG_URI)
+        self.iconview.drag_dest_set_target_list(targets)
+
+    def on_about_activate(self, menuitem):
+        dialog = AboutDialog(self.window)
+        dialog.run()
+        dialog.destroy()
     
-    if items:
-        current_it = model.get_iter(items[0])
-        (title, desc, tags, thumb) = model.get(current_it,
-                                               COL_TITLE,
-                                               COL_DESCRIPTION,
-                                               COL_TAGS,
-                                               COL_THUMBNAIL)
+    def on_upload_activate(self, menuitem):
+        it = self.model.get_iter_first()
+        # If we have some pictures, disable the iconview
+        if it is not None:
+            # TODO: disable upload menu item
+            self.iconview.set_sensitive(False)
         
-        title_entry.set_sensitive(True)
-        title_entry.set_text(title)
-        desc_entry.set_sensitive(True)
-        desc_entry.set_text(desc)
-        tags_entry.set_sensitive(True)
-        tags_entry.set_text(tags)
-
-        thumbnail_image.set_from_pixbuf(thumb)
-    else:
-        current_it = None
-        title_entry.set_sensitive(False)
-        title_entry.set_text("")
-        desc_entry.set_sensitive(False)
-        desc_entry.set_text("")
-        tags_entry.set_sensitive(False)
-        tags_entry.set_text("")
-        
-        thumbnail_image.set_from_pixbuf(None)
-
-iconview.connect('selection-changed', on_selection_changed)
-
-def get_thumb_size(width, height):
-    ratio = width/float(height)
-    if width > height:
-        return (64, int(64/ratio))
-    else:
-        return (int(64/ratio), 64)
-
-def on_drop(widget, context, x, y, selection, targetType, timestamp):
-    if targetType == DRAG_IMAGE:
-        pixbuf = selection.get_pixbuf()
-        sizes = get_thumb_size (pixbuf.get_width(), pixbuf.get_height())
-        thumb = pixbuf.scale_simple(sizes[0], sizes[1], gtk.gdk.INTERP_BILINEAR)
-        model.set(model.append(),
-                  COL_IMAGE, pixbuf,
-                  COL_URI, None,
-                  COL_THUMBNAIL, thumb,
-                  COL_TITLE, "",
-                  COL_DESCRIPTION, "",
-                  COL_TAGS, "")
+        while it is not None:
+            (uri, pixbuf, title, desc, tags) = self.model.get(it, COL_URI, COL_IMAGE, COL_TITLE, COL_DESCRIPTION, COL_TAGS)
     
-    elif targetType == DRAG_URI:
-        for uri in selection.get_uris():
+            t = UploadTask()
+            t.uri = uri
+            t.pixbuf = pixbuf
+            t.title = title
+            t.description = desc
+            t.tags = tags
+    
+            upload_queue.put(t)
             
-            # TODO: MIME type check
+            it = self.model.iter_next(it)
+    
+    def on_selection_changed(self, iconview):
+        global current_it
+        
+        items = iconview.get_selected_items()
+        if len(items) > 1:
+            print "Unexpected number of selections"
+            return
+        
+        if items:
+            current_it = self.model.get_iter(items[0])
+            (title, desc, tags, thumb) = self.model.get(current_it,
+                                                   COL_TITLE,
+                                                   COL_DESCRIPTION,
+                                                   COL_TAGS,
+                                                   COL_THUMBNAIL)
             
-            # TODO: use gnome-vfs to handle remote files
-            filename = urlparse(uri)[2]
-
-            exif = EXIF.process_file(open(filename, 'rb'))
-
-            thumb = exif.get('JPEGThumbnail', None)
-            if thumb:
-                loader = gtk.gdk.PixbufLoader()
-                def on_size_prepared(loader, width, height):
-                    loader.set_size(*get_thumb_size(width, height))
-                loader.connect('size-prepared', on_size_prepared)
-                loader.write(thumb)
-                loader.close()
-                thumb = loader.get_pixbuf()
-                # TODO: rotate if required?
-            else:
-                thumb = gtk.gdk.pixbuf_new_from_file_at_size (filename, 64, 64)
-
-            # Extra useful data from image
-            title = os.path.splitext(os.path.basename(filename))[0] # TODO: more
-            desc = exif.get('Image ImageDescription', "")
+            self.title_entry.set_sensitive(True)
+            self.title_entry.set_text(title)
+            self.desc_entry.set_sensitive(True)
+            self.desc_entry.set_text(desc)
+            self.tags_entry.set_sensitive(True)
+            self.tags_entry.set_text(tags)
+    
+            self.thumbnail_image.set_from_pixbuf(thumb)
+        else:
+            current_it = None
+            self.title_entry.set_sensitive(False)
+            self.title_entry.set_text("")
+            self.desc_entry.set_sensitive(False)
+            self.desc_entry.set_text("")
+            self.tags_entry.set_sensitive(False)
+            self.tags_entry.set_text("")
             
-            model.set(model.append(),
-                      COL_URI, uri,
-                      COL_IMAGE, None,
+            self.thumbnail_image.set_from_pixbuf(None)
+
+    @staticmethod
+    def get_thumb_size(width, height):
+        ratio = width/float(height)
+        if width > height:
+            return (64, int(64/ratio))
+        else:
+            return (int(64/ratio), 64)
+    
+    def on_drag_data_received(self, widget, context, x, y, selection, targetType, timestamp):
+        if targetType == DRAG_IMAGE:
+            pixbuf = selection.get_pixbuf()
+            sizes = self.get_thumb_size (pixbuf.get_width(), pixbuf.get_height())
+            thumb = pixbuf.scale_simple(sizes[0], sizes[1], gtk.gdk.INTERP_BILINEAR)
+            self.model.set(self.model.append(),
+                      COL_IMAGE, pixbuf,
+                      COL_URI, None,
                       COL_THUMBNAIL, thumb,
-                      COL_TITLE, title,
-                      COL_DESCRIPTION, desc,
+                      COL_TITLE, "",
+                      COL_DESCRIPTION, "",
                       COL_TAGS, "")
-    else:
-        print "Unhandled target type %d" % targetType
+        
+        elif targetType == DRAG_URI:
+            for uri in selection.get_uris():
+                
+                # TODO: MIME type check
+                
+                # TODO: use gnome-vfs to handle remote files
+                filename = urlparse(uri)[2]
     
-    context.finish(True, True, timestamp)
+                exif = EXIF.process_file(open(filename, 'rb'))
+    
+                thumb = exif.get('JPEGThumbnail', None)
+                if thumb:
+                    loader = gtk.gdk.PixbufLoader()
+                    def on_size_prepared(loader, width, height):
+                        loader.set_size(*self.get_thumb_size(width, height))
+                    loader.connect('size-prepared', on_size_prepared)
+                    loader.write(thumb)
+                    loader.close()
+                    thumb = loader.get_pixbuf()
+                    # TODO: rotate if required?
+                else:
+                    thumb = gtk.gdk.pixbuf_new_from_file_at_size (filename, 64, 64)
+    
+                # Extra useful data from image
+                title = os.path.splitext(os.path.basename(filename))[0] # TODO: more
+                desc = exif.get('Image ImageDescription', "")
+                
+                self.model.set(self.model.append(),
+                          COL_URI, uri,
+                          COL_IMAGE, None,
+                          COL_THUMBNAIL, thumb,
+                          COL_TITLE, title,
+                          COL_DESCRIPTION, desc,
+                          COL_TAGS, "")
+        else:
+            print "Unhandled target type %d" % targetType
+        
+        context.finish(True, True, timestamp)
 
-iconview.connect('drag-data-received', on_drop)
+    def done(self):
+        gtk.gdk.threads_enter()
+        self.model.clear()
+        self.iconview.set_sensitive(True)
+        # TODO: enable upload menu item
+        gtk.gdk.threads_leave()
 
 class UploadTask:
     uri = None
@@ -188,22 +238,13 @@ class UploadTask:
     title = None
     description = None
     tags = None
-    
-from Queue import Queue
-upload_queue = Queue()
 
-# Called when the uploading is done, to empty the model and unlock the view.
-def done():
-    gtk.gdk.threads_enter()
-    model.clear()
-    iconview.set_sensitive(True)
-    # TODO: enable upload menu item
-    gtk.gdk.threads_leave()
 
 class Uploader(threading.Thread):
-    def __init__(self):
+    def __init__(self, postr):
         threading.Thread.__init__(self)
         self.setDaemon(True)
+        self.postr = postr
         
     def run(self):
         while 1:
@@ -230,45 +271,12 @@ class Uploader(threading.Thread):
                 print fapi.getPrintableError(rsp)
 
             if upload_queue.empty():
-                done()
+                self.postr.done()
 
-def on_upload_activate(menuitem):
-    it = model.get_iter_first()
-    # If we have some pictures, disable the iconview
-    if it is not None:
-        # TODO: disable upload menu item
-        iconview.set_sensitive(False)
-    
-    while it is not None:
-        (uri, pixbuf, title, desc, tags) = model.get(it, COL_URI, COL_IMAGE, COL_TITLE, COL_DESCRIPTION, COL_TAGS)
 
-        t = UploadTask()
-        t.uri = uri
-        t.pixbuf = pixbuf
-        t.title = title
-        t.description = desc
-        t.tags = tags
-
-        upload_queue.put(t)
-        
-        it = model.iter_next(it)
-
-glade.get_widget('upload').connect('activate', on_upload_activate)
-
-def on_about_activate(menuitem):
-    dialog = gtk.AboutDialog()
-    dialog.set_transient_for(window)
-    dialog.set_name('Flickr Uploader')
-    dialog.set_copyright(u'Copyright \u00A9 2006 Ross Burton')
-    dialog.set_authors(('Ross Burton <ross@burtonini.com>',))
-    dialog.set_website('http://burtonini.com/')
-    dialog.run()
-    dialog.destroy()
-
-glade.get_widget('about').connect('activate', on_about_activate)
-
-Uploader().start()
-window.show()
 
 if __name__ == "__main__":
+    p = Postr()
+    Uploader(p).start()
+    p.window.show()
     gtk.main()
