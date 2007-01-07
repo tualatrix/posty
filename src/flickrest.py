113177a973a0d8e225a8cbe0d2538ff9489050ad
Ross Burton <ross@burtonini.com>
2007-01-07 20:53:15 +0000

    Update flickrest

--- src/flickrest.py
+++ src/flickrest.py
@@ -1,3 +1,20 @@
+# flickrpc -- a Flickr client library.
+#
+# Copyright (C) 2007 Ross Burton <ross@burtonini.com>
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+# St, Fifth Floor, Boston, MA 02110-1301 USA
+
 import logging, md5, os, mimetools, urllib
 from twisted.internet import defer
 from twisted.python.failure import Failure
