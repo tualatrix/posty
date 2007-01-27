5e6f27544ce322217833f4bc06c855f2f931154d
Ross Burton <ross@burtonini.com>
2007-01-27 13:08:49 +0000

    Work with Python 2.5 (rev 44)

--- src/flickrest.py
+++ src/flickrest.py
@@ -19,7 +19,10 @@ import logging, md5, os, mimetools, urllib
 from twisted.internet import defer
 from twisted.python.failure import Failure
 from twisted.web import client
-from elementtree import ElementTree
+try:
+    from xml.etree import ElementTree
+except ImportError:
+    from elementtree import ElementTree
 
 class FlickrError(Exception):
     def __init__(self, code, message):
