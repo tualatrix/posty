9013a344a66449fe21770d61e91c18308b52ffac
Ross Burton <ross@burtonini.com>
2007-04-06 12:20:53 +0100

    Add rocking icon from Andreas Nilsson

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from glob import glob
 
 setup(name='Postr',
       version='0.5',
@@ -14,6 +15,12 @@ setup(name='Postr',
       packages=['postr'],
       package_data={'postr': ['postr.glade']},
       data_files=[('share/applications', ['src/postr.desktop']),
-                  ('lib/nautilus/extensions-1.0/python', ['nautilus/postrExtension.py'])
-                  ]
+                  ('lib/nautilus/extensions-1.0/python', ['nautilus/postrExtension.py']),
+                  ('share/icons/hicolor/16x16/apps', glob('icons/16x16/*')),
+                  ('share/icons/hicolor/22x22/apps', glob('icons/22x22/*')),
+                  ('share/icons/hicolor/32x32/apps', glob('icons/32x32/*')),
+                  ('share/icons/hicolor/scalable/apps', glob('icons/scalable/*')),
+                  ],
+      
       )
+
