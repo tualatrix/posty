97ac078b91703f3d2e0efd17a12c3aa168ec1ec4
Ross Burton <ross@burtonini.com>
2008-01-19 14:07:35 +0000

    Update (C) dates

--- src/AboutDialog.py
+++ src/AboutDialog.py
@@ -22,7 +22,7 @@ class AboutDialog(gtk.AboutDialog):
     def __init__(self):
         gtk.AboutDialog.__init__(self)
         self.set_name(_('Flickr Uploader'))
-        self.set_copyright(u'Copyright \u00A9 2006 Ross Burton')
+        self.set_copyright(u'Copyright \u00A9 2006-2008 Ross Burton')
         self.set_authors(('Ross Burton <ross@burtonini.com>',))
         self.set_website('http://burtonini.com/')
         self.set_logo_icon_name('postr')
