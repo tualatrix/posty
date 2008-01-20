a91286d491fed7717335cca250a793577a9e3b87
Ross Burton <ross@burtonini.com>
2008-01-20 14:39:22 +0000

    Use names for columns

--- src/GroupSelector.py
+++ src/GroupSelector.py
@@ -2,10 +2,14 @@ import gobject, gtk, pango
 from ErrorDialog import ErrorDialog
 import util
 
+(COL_SELECTED,
+ COL_NAME,
+ COL_ICON) = range(0, 3)
+
 class GroupSelector(gtk.TreeView):
     def __init__(self, flickr):
         self.flickr = flickr
-        self.model = gtk.ListStore(gobject.TYPE_STRING, gtk.gdk.Pixbuf)
+        self.model = gtk.ListStore(gobject.TYPE_BOOLEAN, gobject.TYPE_STRING, gtk.gdk.Pixbuf)
         gtk.TreeView.__init__(self, self.model)
         self.set_headers_visible(False)
         
@@ -14,15 +18,14 @@ class GroupSelector(gtk.TreeView):
         
         renderer =  gtk.CellRendererToggle()
         column.pack_start(renderer, False)
-        #column.add_attribute(renderer, "text", 0)
         
         renderer =  gtk.CellRendererPixbuf()
         column.pack_start(renderer, False)
-        column.add_attribute(renderer, "pixbuf", 1)
+        column.add_attribute(renderer, "pixbuf", COL_ICON)
         
         renderer =  gtk.CellRendererText()
         column.pack_start(renderer, True)
-        column.add_attribute(renderer, "text", 0)
+        column.add_attribute(renderer, "text", COL_NAME)
 
     def update(self):
         self.flickr.groups_pools_getGroups().addCallbacks(self.got_groups, self.twisted_error)
@@ -31,9 +34,9 @@ class GroupSelector(gtk.TreeView):
         from elementtree.ElementTree import dump
         for group in rsp.findall("groups/group"):
             it = self.model.append()
-            self.model.set (it, 0, group.get("name"))
+            self.model.set (it, COL_NAME, group.get("name"))
             def got_thumb(thumb, it):
-                self.model.set (it, 1, thumb)
+                self.model.set (it, COL_ICON, thumb)
             util.get_buddyicon(self.flickr, group, 24).addCallback(got_thumb, it)
         
     def twisted_error(self, failure):
