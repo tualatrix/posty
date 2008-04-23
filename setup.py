293f3bb5ce48ff0577d9eeeb41f82bd030464212
Ross Burton <ross@burtonini.com>
2008-04-23 10:06:40 +0100

    Install nautilus extension to extension-1.0 and extension-2.0

--- setup.py
+++ setup.py
@@ -16,15 +16,18 @@ setup(name='Postr',
       packages=['postr'],
       package_data={'postr': ['postr.glade']},
       data_files=[('share/applications', ['data/postr.desktop']),
-                  ('lib/nautilus/extensions-1.0/python', ['nautilus/postrExtension.py']),
                   ('share/icons/hicolor/16x16/apps', glob('data/16x16/*.png')),
                   ('share/icons/hicolor/22x22/apps', glob('data/22x22/*.png')),
                   ('share/icons/hicolor/24x24/apps', glob('data/24x24/*.png')),
                   ('share/icons/hicolor/32x32/apps', glob('data/32x32/*.png')),
                   ('share/icons/hicolor/scalable/apps', glob('data/scalable/*.svg')),
+                  # TODO: inspect nautilus-python.pc to get path
+                  ('lib/nautilus/extensions-1.0/python', ['nautilus/postrExtension.py']),
+                  ('lib/nautilus/extensions-2.0/python', ['nautilus/postrExtension.py']),
                   ],
       
       )
 
 # TODO: install translations
 # TODO: update icon cache
+# TODO: rewrite in autotools because this is getting silly
