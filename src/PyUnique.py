9388d023d3835b239940cb0b80f5ff382ef00274
Karl Mikaelsson <derfian@lysator.liu.se>
2010-02-03 09:00:11 +0100

    Modify postr ever so slightly to accomodate for using pyunique

--- src/PyUnique.py
+++ src/PyUnique.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8; mode: python -*-
+#
 # PyUnique, a pure python reimplementation of unique, a
 # single-instance application library.
 #
@@ -16,7 +17,8 @@
 # You should have received a copy of the GNU General Public License along with
 # this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
 # St, Fifth Floor, Boston, MA 02110-1301 USA
-
+#
+#
 
 import gobject
 import dbus
