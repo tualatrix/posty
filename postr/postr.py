c4cdc785d30263645391af585e1ceb36afddeb14
TualatriX <tualatrix@gmail.com>
2010-09-08 20:46:47 +0800

    Remove the unneeded import

--- postr/postr.py
+++ postr/postr.py
@@ -15,7 +15,7 @@
 # this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
 # St, Fifth Floor, Boston, MA 02110-1301 USA
 
-import logging, os, urllib
+import os, urllib
 from urlparse import urlparse
 from os.path import basename
 
